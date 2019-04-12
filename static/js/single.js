b41c0c22f0b2a1ef29b7ccdf7f0f3e2b56465ed8
FILES=0 <files.eq.zero@gmail.com>
2019-04-12 13:30:10 +0900

    code with filename

--- static/js/single.js
+++ static/js/single.js
@@ -1,4 +1,5 @@
 (function () {
+    // generate header link
     function createLink(aId) {
         var t = document.createElement("a");
         t.className = "fa fa-link header-link ";
@@ -17,5 +18,22 @@
 
     for (var t = 2; t <= 4; t++) {
         appendHeaderLink(t)
+    }
+
+    // code block filename
+    var codeBlocks = document.body.getElementsByClassName("highlight");
+
+    for (i = 0; i <= codeBlocks.length - 1; i++) {
+        var code = codeBlocks[i].getElementsByTagName("code");
+        if (code) {
+            var codeName = code[0].className.split(":")[1];
+            if (codeName) {
+                var div = document.createElement('div');
+                div.textContent = codeName;
+                div.classList.add('code-name');
+                //codeBlocks[i].insertBefore(div, codeBlocks[i].firstElementChild);
+                codeBlocks[i].insertBefore(div, codeBlocks[i].firstElementChild);
+            }
+        }
     }
 })();
